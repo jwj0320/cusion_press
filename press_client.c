29c6a3783311a1dc6afd855c492c57a6d05b02b1
minhu kang <maybe@ajou.ac.kr>
2021-12-13 09:52:19 +0000

    Update press_client.c

--- press_client.c
+++ press_client.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port = htons(atoi(port));
+    serv_addr.sin_port = htons(port);
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
         error_handling("connect() error");
